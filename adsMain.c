5a1360814fc8c2dff6a2ed8f3b26c53e0e287612
Lukas Naruševičius <narusevic@gmail.com>
2017-03-27 12:48:26 +0300

    first

---
+++ adsMain.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "bigInteger.h"
+#include "Stack.h"
+
+int main ()
+{
+	if (create (&st) == 0)
+    {
+		printf ("Stack has been created.\n");
+    }
+	else
+    {
+        printf ("Stack is already created.\n");
+    }
+
+    printf("labas");
+
+
+    return 0;
+}
